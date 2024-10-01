b44410cfe4a7a1f395b34af5fbdc0747fa96ce1b
github-action-benchmark <github@users.noreply.github.com>
2024-10-01 22:45:44 +0000

    add Haskell Benchmark (customSmallerIsBetter) benchmark result for 28abc8e197cbcc5ab6201239a87cfb5899f336f7

---
+++ dev/bench/data.js
@@ -0,0 +1,368 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1727822743820,
+  "repoUrl": "https://github.com/sgillespie/cardano-ledger",
+  "entries": {
+    "Haskell Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "sean.gillespie@iohk.io",
+            "name": "Sean D Gillespie",
+            "username": "sgillespie"
+          },
+          "committer": {
+            "email": "sean.gillespie@iohk.io",
+            "name": "Sean D Gillespie",
+            "username": "sgillespie"
+          },
+          "distinct": true,
+          "id": "c3fb1068c172d20999cef211729d4d71589cdb5b",
+          "message": "Export unsafe constructors from Internal modules",
+          "timestamp": "2024-09-26T12:18:33-04:00",
+          "tree_id": "3247e08af1766a46d3094ceae9dca14d0993de94",
+          "url": "https://github.com/sgillespie/cardano-ledger/commit/c3fb1068c172d20999cef211729d4d71589cdb5b"
+        },
+        "date": 1727368593187,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "applyTxBenchmarks/ApplyTxInEra/ShelleyEra C_Crypto",
+            "value": 0.0000554962560457954,
+            "unit": "Nanoseconds",
+            "range": 0.0000012411653827092853
+          },
+          {
+            "name": "applyTxBenchmarks/ApplyTxInEra/AllegraEra C_Crypto",
+            "value": 0.00005926343536332936,
+            "unit": "Nanoseconds",
+            "range": 6.233587280660356e-7
+          },
+          {
+            "name": "applyTxBenchmarks/ApplyTxInEra/MaryEra C_Crypto",
+            "value": 0.00006815413456305182,
+            "unit": "Nanoseconds",
+            "range": 5.531349642670164e-7
+          },
+          {
+            "name": "applyTxBenchmarks/ApplyTxInEra/AlonzoEra C_Crypto",
+            "value": 0.0001387338568011436,
+            "unit": "Nanoseconds",
+            "range": 0.000001145487827172749
+          },
+          {
+            "name": "applyTxBenchmarks/Deserialise Shelley Tx/ShelleyEra C_Crypto",
+            "value": 0.000009806247201977344,
+            "unit": "Nanoseconds",
+            "range": 1.115468504178558e-7
+          },
+          {
+            "name": "applyTxBenchmarks/Deserialise Shelley Tx/AllegraEra C_Crypto",
+            "value": 0.00001751208983747362,
+            "unit": "Nanoseconds",
+            "range": 1.615962652466861e-7
+          },
+          {
+            "name": "applyTxBenchmarks/Deserialise Shelley Tx/MaryEra C_Crypto",
+            "value": 0.000018431940785619757,
+            "unit": "Nanoseconds",
+            "range": 1.7722570002762833e-7
+          },
+          {
+            "name": "applyTxBenchmarks/Deserialise Shelley Tx/AlonzoEra C_Crypto",
+            "value": 0.000013135213256910016,
+            "unit": "Nanoseconds",
+            "range": 2.450436174990124e-7
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sean.gillespie@iohk.io",
+            "name": "Sean D Gillespie",
+            "username": "sgillespie"
+          },
+          "committer": {
+            "email": "sean.gillespie@iohk.io",
+            "name": "Sean D Gillespie",
+            "username": "sgillespie"
+          },
+          "distinct": true,
+          "id": "77948713ba56b57036da1956347f607f30209d16",
+          "message": "Export unsafe constructors from Internal modules",
+          "timestamp": "2024-09-26T12:30:27-04:00",
+          "tree_id": "8123b1a2b6937f789154a81a6a0e35c9854802a0",
+          "url": "https://github.com/sgillespie/cardano-ledger/commit/77948713ba56b57036da1956347f607f30209d16"
+        },
+        "date": 1727369246491,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "applyTxBenchmarks/ApplyTxInEra/ShelleyEra C_Crypto",
+            "value": 0.00005359560151054843,
+            "unit": "Nanoseconds",
+            "range": 4.7153230359345725e-7
+          },
+          {
+            "name": "applyTxBenchmarks/ApplyTxInEra/AllegraEra C_Crypto",
+            "value": 0.000057486190698311314,
+            "unit": "Nanoseconds",
+            "range": 0.000002163086444097266
+          },
+          {
+            "name": "applyTxBenchmarks/ApplyTxInEra/MaryEra C_Crypto",
+            "value": 0.00006706928873250673,
+            "unit": "Nanoseconds",
+            "range": 4.441881671357363e-7
+          },
+          {
+            "name": "applyTxBenchmarks/ApplyTxInEra/AlonzoEra C_Crypto",
+            "value": 0.00013804747309557422,
+            "unit": "Nanoseconds",
+            "range": 8.798770804099917e-7
+          },
+          {
+            "name": "applyTxBenchmarks/Deserialise Shelley Tx/ShelleyEra C_Crypto",
+            "value": 0.000009608064590676471,
+            "unit": "Nanoseconds",
+            "range": 1.1428823585016745e-7
+          },
+          {
+            "name": "applyTxBenchmarks/Deserialise Shelley Tx/AllegraEra C_Crypto",
+            "value": 0.000017556704011402058,
+            "unit": "Nanoseconds",
+            "range": 7.353918222321489e-7
+          },
+          {
+            "name": "applyTxBenchmarks/Deserialise Shelley Tx/MaryEra C_Crypto",
+            "value": 0.000018071986450541986,
+            "unit": "Nanoseconds",
+            "range": 1.6545145847239857e-7
+          },
+          {
+            "name": "applyTxBenchmarks/Deserialise Shelley Tx/AlonzoEra C_Crypto",
+            "value": 0.000012545044450216978,
+            "unit": "Nanoseconds",
+            "range": 1.30266455484559e-7
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sean.gillespie@iohk.io",
+            "name": "Sean D Gillespie",
+            "username": "sgillespie"
+          },
+          "committer": {
+            "email": "sean@mistersg.net",
+            "name": "Sean Gillespie",
+            "username": "sgillespie"
+          },
+          "distinct": true,
+          "id": "463ca611f0b9cea5a68de4b0e8b9b4d1359dbf1e",
+          "message": "Export unsafe constructors from Internal modules",
+          "timestamp": "2024-09-30T10:53:01-04:00",
+          "tree_id": "c99a5ccfb6d7e3fde6a095899837c54add8ee17a",
+          "url": "https://github.com/sgillespie/cardano-ledger/commit/463ca611f0b9cea5a68de4b0e8b9b4d1359dbf1e"
+        },
+        "date": 1727709047794,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "applyTxBenchmarks/ApplyTxInEra/ShelleyEra C_Crypto",
+            "value": 0.00005351781128053028,
+            "unit": "Nanoseconds",
+            "range": 7.005777960046307e-7
+          },
+          {
+            "name": "applyTxBenchmarks/ApplyTxInEra/AllegraEra C_Crypto",
+            "value": 0.000058337856286586614,
+            "unit": "Nanoseconds",
+            "range": 8.461484728663882e-7
+          },
+          {
+            "name": "applyTxBenchmarks/ApplyTxInEra/MaryEra C_Crypto",
+            "value": 0.00006721094026391358,
+            "unit": "Nanoseconds",
+            "range": 6.012804663186652e-7
+          },
+          {
+            "name": "applyTxBenchmarks/ApplyTxInEra/AlonzoEra C_Crypto",
+            "value": 0.0001209193320428067,
+            "unit": "Nanoseconds",
+            "range": 0.0000014245991274895393
+          },
+          {
+            "name": "applyTxBenchmarks/Deserialise Shelley Tx/ShelleyEra C_Crypto",
+            "value": 0.000009851975655595505,
+            "unit": "Nanoseconds",
+            "range": 1.7558220882724303e-7
+          },
+          {
+            "name": "applyTxBenchmarks/Deserialise Shelley Tx/AllegraEra C_Crypto",
+            "value": 0.000017577042140578175,
+            "unit": "Nanoseconds",
+            "range": 1.3937203912332908e-7
+          },
+          {
+            "name": "applyTxBenchmarks/Deserialise Shelley Tx/MaryEra C_Crypto",
+            "value": 0.000017992131660285274,
+            "unit": "Nanoseconds",
+            "range": 1.847530646640371e-7
+          },
+          {
+            "name": "applyTxBenchmarks/Deserialise Shelley Tx/AlonzoEra C_Crypto",
+            "value": 0.000009786145176698226,
+            "unit": "Nanoseconds",
+            "range": 8.458251290173598e-8
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sean.gillespie@iohk.io",
+            "name": "Sean D Gillespie",
+            "username": "sgillespie"
+          },
+          "committer": {
+            "email": "sean@mistersg.net",
+            "name": "Sean Gillespie",
+            "username": "sgillespie"
+          },
+          "distinct": true,
+          "id": "a9f6b4d24065dee1ce22ba21caadc56a5e2ee405",
+          "message": "Export unsafe constructors from Internal modules",
+          "timestamp": "2024-10-01T13:46:30-04:00",
+          "tree_id": "e0b6ed0290f436a164bce157bfd7e3794f25f355",
+          "url": "https://github.com/sgillespie/cardano-ledger/commit/a9f6b4d24065dee1ce22ba21caadc56a5e2ee405"
+        },
+        "date": 1727805690644,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "applyTxBenchmarks/ApplyTxInEra/ShelleyEra C_Crypto",
+            "value": 0.000056197164613063,
+            "unit": "Nanoseconds",
+            "range": 0.000007054596466774475
+          },
+          {
+            "name": "applyTxBenchmarks/ApplyTxInEra/AllegraEra C_Crypto",
+            "value": 0.000057967034991001086,
+            "unit": "Nanoseconds",
+            "range": 7.137648239642234e-7
+          },
+          {
+            "name": "applyTxBenchmarks/ApplyTxInEra/MaryEra C_Crypto",
+            "value": 0.00006706763810138223,
+            "unit": "Nanoseconds",
+            "range": 4.956542455897843e-7
+          },
+          {
+            "name": "applyTxBenchmarks/ApplyTxInEra/AlonzoEra C_Crypto",
+            "value": 0.00012443637353467266,
+            "unit": "Nanoseconds",
+            "range": 0.00001596717239113408
+          },
+          {
+            "name": "applyTxBenchmarks/Deserialise Shelley Tx/ShelleyEra C_Crypto",
+            "value": 0.000009611289387755107,
+            "unit": "Nanoseconds",
+            "range": 1.018731538200885e-7
+          },
+          {
+            "name": "applyTxBenchmarks/Deserialise Shelley Tx/AllegraEra C_Crypto",
+            "value": 0.000017449019165949506,
+            "unit": "Nanoseconds",
+            "range": 1.7834680363870273e-7
+          },
+          {
+            "name": "applyTxBenchmarks/Deserialise Shelley Tx/MaryEra C_Crypto",
+            "value": 0.00001788854020206244,
+            "unit": "Nanoseconds",
+            "range": 1.466143414491434e-7
+          },
+          {
+            "name": "applyTxBenchmarks/Deserialise Shelley Tx/AlonzoEra C_Crypto",
+            "value": 0.000009568904035522626,
+            "unit": "Nanoseconds",
+            "range": 8.10506154225802e-8
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sean.gillespie@iohk.io",
+            "name": "Sean D Gillespie",
+            "username": "sgillespie"
+          },
+          "committer": {
+            "email": "alexey.kuleshevich@iohk.io",
+            "name": "Alexey Kuleshevich",
+            "username": "lehins"
+          },
+          "distinct": true,
+          "id": "28abc8e197cbcc5ab6201239a87cfb5899f336f7",
+          "message": "Export unsafe constructors from Internal modules",
+          "timestamp": "2024-10-01T16:29:39-06:00",
+          "tree_id": "b0d13970d2b8f60ba7576984b97cad57fe05384a",
+          "url": "https://github.com/sgillespie/cardano-ledger/commit/28abc8e197cbcc5ab6201239a87cfb5899f336f7"
+        },
+        "date": 1727822728359,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "applyTxBenchmarks/ApplyTxInEra/ShelleyEra C_Crypto",
+            "value": 0.00005409396165945296,
+            "unit": "Nanoseconds",
+            "range": 0.0000018483197214505368
+          },
+          {
+            "name": "applyTxBenchmarks/ApplyTxInEra/AllegraEra C_Crypto",
+            "value": 0.00005799842192599678,
+            "unit": "Nanoseconds",
+            "range": 3.5075667553740917e-7
+          },
+          {
+            "name": "applyTxBenchmarks/ApplyTxInEra/MaryEra C_Crypto",
+            "value": 0.00006677118815668252,
+            "unit": "Nanoseconds",
+            "range": 4.2459776093786024e-7
+          },
+          {
+            "name": "applyTxBenchmarks/ApplyTxInEra/AlonzoEra C_Crypto",
+            "value": 0.00012335185762590395,
+            "unit": "Nanoseconds",
+            "range": 6.62574534531567e-7
+          },
+          {
+            "name": "applyTxBenchmarks/Deserialise Shelley Tx/ShelleyEra C_Crypto",
+            "value": 0.000010213283947887628,
+            "unit": "Nanoseconds",
+            "range": 2.2938687430210654e-7
+          },
+          {
+            "name": "applyTxBenchmarks/Deserialise Shelley Tx/AllegraEra C_Crypto",
+            "value": 0.000018091337805635356,
+            "unit": "Nanoseconds",
+            "range": 1.968271470710364e-7
+          },
+          {
+            "name": "applyTxBenchmarks/Deserialise Shelley Tx/MaryEra C_Crypto",
+            "value": 0.000018678535381970846,
+            "unit": "Nanoseconds",
+            "range": 1.0993709361740068e-7
+          },
+          {
+            "name": "applyTxBenchmarks/Deserialise Shelley Tx/AlonzoEra C_Crypto",
+            "value": 0.000009789998676964763,
+            "unit": "Nanoseconds",
+            "range": 1.4155481207643899e-7
+          }
+        ]
+      }
+    ]
+  }
+}
